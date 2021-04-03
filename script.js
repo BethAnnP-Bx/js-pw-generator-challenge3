fcd83e1a2f4513328ce602b443714eef156b0d23
Beth Ann Patterson <patterson.beth.ann@gmail.com>
2021-04-03 12:11:09 -0700

    added psuedo-code to js to plan pw generator

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-//Pseudo-code here
+// Pseudo-code starts here
+// pw criteria is in this order per the acceptance criteria: Length of pw, lowercase, uppercase, numeric, special characters
+
+// Prompt "Choose the length of your unique password. Input a number between 8-128 for number of characters."
+// If length is outside of 8-128 characters Alert "Do enter a number between 8-128."
+// Alert to validate "Your unique password will be this number of characters."
+
+// Prompt "Do you want lowercase letters in your unique password?"
+// Alert to validate with either "Your unique password will include or not include lowercase letters."
+
+// Prompt "Do you want uppercase letters in your unique password?"
+// Alert to validate with either "Your unique password will include or not include uppercase letters."
+
+// Prompt "Do you want numbers in your unique password?"
+// Alert to validate with either "Your unique password will include or not include numbers."
+
+// Prompt "Do you want special characters in your unique password?"
+// Alert to validate with either "Your unique password will include or not include special characters."
+
+// Unique password is written to the page within the card-body placeholder.
+
+// Use for loop, Math.floor, Math.random
+// Pseudo-code ends here
 
 // Assignment code here
 
